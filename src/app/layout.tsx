dd7372009e19ed750906c1b58779f859a6ae3f9b
Cosi Leone <cosi.leone@icloud.com>
2023-09-21 16:11:45 +0100

    fix title

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,37 +1,38 @@
-import { Inter } from 'next/font/google'
-import clsx from 'clsx'
+import { Inter } from "next/font/google";
+import clsx from "clsx";
 
-import '~/styles/globals.css'
-import { type Metadata } from 'next'
+import "~/styles/globals.css";
+import { type Metadata } from "next";
 
 const inter = Inter({
-  subsets: ['latin'],
-  display: 'swap',
-  variable: '--font-inter',
-})
+  subsets: ["latin"],
+  display: "swap",
+  variable: "--font-inter",
+});
 
 export const metadata: Metadata = {
   title: {
-    template: '%s - Pocket',
-    default: 'Pocket - Invest at the perfect time.',
+    template: "%s - BOK",
+    default:
+      "BOK - L’app che rende l’indipendenza finanziaria un gioco da ragazzi!",
   },
   description:
-    'By leveraging insights from our network of industry insiders, you’ll know exactly when to buy to maximize profit, and exactly when to sell to avoid painful losses.',
-}
+    "Raggiungi la tua indipendenza finanziaria con BOK risparmia ed investi, creato dalla Generazione Z per la Generazione Z",
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html
       lang="en"
-      className={clsx('h-full bg-gray-50 antialiased', inter.variable)}
+      className={clsx("h-full bg-gray-50 antialiased", inter.variable)}
     >
       <body className="flex h-full flex-col">
         <div className="flex min-h-full flex-col">{children}</div>
       </body>
     </html>
-  )
+  );
 }
