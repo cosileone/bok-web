7da25faf7f92df438a218b71cfaa86fe193a049e
Cosi Leone <cosi.leone@icloud.com>
2024-02-21 04:35:23 +0100

    minor tweaks

--- src/app/onboarding/quiz/page.tsx
+++ src/app/onboarding/quiz/page.tsx
@@ -3,14 +3,14 @@ import Quiz from "~/components/Quiz";
 
 const QuizPage = () => {
   return (
-    <div>
+    <>
       {/*<UserButton afterSignOutUrl={"/"} />*/}
       {/*<SignOutButton />*/}
       {/*<h1>Quiz Page</h1>*/}
       <div className={"flex justify-center pt-20 sm:pt-40"}>
         <Quiz />
       </div>
-    </div>
+    </>
   );
 };
 
--- src/app/onboarding/results/page.tsx
+++ src/app/onboarding/results/page.tsx
@@ -1,5 +1,9 @@
 const QuizResultsPage = () => {
-  return <>QuizResultsPage</>;
+  return (
+    <>
+      <div className={"flex justify-center pt-20 sm:pt-40"}></div>
+    </>
+  );
 };
 
 export default QuizResultsPage;
