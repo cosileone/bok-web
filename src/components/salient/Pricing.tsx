47c8b8a524b2b06451786c845e27b4a74180c692
Cosi Leone <cosi.leone@icloud.com>
2024-05-15 06:59:54 +0100

    pricing changes

--- src/components/salient/Pricing.tsx
+++ src/components/salient/Pricing.tsx
@@ -105,7 +105,7 @@ function Plan({
         className="mt-8"
         aria-label={`Get started with the ${name} plan for ${price}`}
       >
-        Get started
+        Contattaci
       </Button>
     </section>
   );
@@ -123,57 +123,35 @@ export function Pricing() {
           <h2 className="font-display text-3xl tracking-tight text-white sm:text-4xl">
             <span className="relative whitespace-nowrap">
               <SwirlyDoodle className="absolute left-0 top-1/2 h-[1em] w-full fill-blue-400" />
-              <span className="relative">Simple pricing,</span>
+              <span className="relative">I nostri pacchetti</span>
             </span>{" "}
-            for everyone.
           </h2>
           <p className="mt-4 text-lg text-slate-400">
-            It doesn’t matter what size your business is, our software won’t
-            work well for you.
+            Guardate i nostri diversi abbonamenti per iniziare a creare
           </p>
         </div>
         <div className="-mx-4 mt-16 grid max-w-2xl grid-cols-1 gap-y-10 sm:mx-auto lg:-mx-8 lg:max-w-none lg:grid-cols-3 xl:mx-0 xl:gap-x-8">
           <Plan
-            name="Starter"
-            price="$9"
-            description="Good for anyone who is self-employed and just getting started."
+            name="Base"
+            price="€800 / mese"
+            description="Comincia con un set di 10 nostri video, toccheremo temi di risparmio, tecniche da poter in maniera pratica  e tanti temi con ogni episodio dedicato. Rendi i Gen Z nella tua banca partecipi. Comincia ora!"
             href="/register"
-            features={[
-              "Send 10 quotes and invoices",
-              "Connect up to 2 bank accounts",
-              "Track up to 15 expenses per month",
-              "Manual payroll support",
-              "Export up to 3 reports",
-            ]}
+            features={[]}
           />
           <Plan
             featured
-            name="Small business"
-            price="$15"
-            description="Perfect for small / medium sized businesses."
+            name="Pro"
+            price="€1500 / mese"
+            description="Ottieni un contratto di un anno con una promessa di 15 video contenuti, di cui 5 su commissione e specifici per le tue necessità, in qualsiasi momento dell’anno. Il contratto mensile può essere annullato in qualsiasi momento"
             href="/register"
-            features={[
-              "Send 25 quotes and invoices",
-              "Connect up to 5 bank accounts",
-              "Track up to 50 expenses per month",
-              "Automated payroll support",
-              "Export up to 12 reports",
-              "Bulk reconcile transactions",
-              "Track in multiple currencies",
-            ]}
+            features={[]}
           />
           <Plan
-            name="Enterprise"
-            price="$39"
-            description="For even the biggest enterprise companies."
+            name="Custom"
+            price="€---"
+            description="Siediti a tavolino con il nostro team di esperti di comunicazione e finanza per giovani. Crea una personale richiesta per contenuti su tutti itemi di finanza, risparmi ed investimenti o temi utili per il tuo istituto finanziario, la parola chiave qui è flessibilità!"
             href="/register"
-            features={[
-              "Send unlimited quotes and invoices",
-              "Connect up to 15 bank accounts",
-              "Track up to 200 expenses per month",
-              "Automated payroll support",
-              "Export up to 25 reports, including TPS",
-            ]}
+            features={["Contattaci per un preventivo"]}
           />
         </div>
       </Container>
