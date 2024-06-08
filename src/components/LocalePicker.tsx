81de145848bba60830fb2b33a170dba610c4c0c2
Cosi Leone <cosi.leone@icloud.com>
2024-06-08 22:53:46 +0100

    fix locale picker bug not routing to locale

--- src/components/LocalePicker.tsx
+++ src/components/LocalePicker.tsx
@@ -31,13 +31,16 @@ const LocalePicker = ({
   const completePathname = usePathname();
   const removeLocale = completePathname.replace(`/${locale}`, "");
 
-  const pathname = urlOverride ?? removeLocale.length <= 1 ? "/" : removeLocale;
-
   const [selectedLocale, setSelectedLocale] = useState(locale);
 
   useEffect(() => {
+    const pathname =
+      urlOverride ?? removeLocale.length <= 1
+        ? `/${selectedLocale}`
+        : removeLocale;
+
     if (selectedLocale === locale) return;
-    void router.push(pathname, { locale: selectedLocale });
+    router.push(pathname, { locale: selectedLocale });
   }, [selectedLocale]);
   return (
     <>
