7460b3e3cf40e6d97783a4f9ad8ab60f3a575f59
Cosi Leone <cosi.leone@icloud.com>
2024-08-24 17:06:44 +0100

    fix faqs

--- src/components/salient/Faqs.tsx
+++ src/components/salient/Faqs.tsx
@@ -1,7 +1,4 @@
-import Image from "next/image";
-
 import { Container } from "~/components/salient/Container";
-import backgroundImage from "~/images/salient/background-faqs.jpg";
 
 const faqsOld = [
   [
@@ -116,25 +113,25 @@ export function Faqs() {
     <section
       id="faq"
       aria-labelledby="faq-title"
-      className="relative overflow-hidden bg-slate-50 py-20 sm:py-32"
+      className="bg-bokpurple relative overflow-hidden py-20 sm:py-32"
     >
-      <Image
-        className="absolute left-1/2 top-0 max-w-none -translate-y-1/4 translate-x-[-30%]"
-        src={backgroundImage}
-        alt=""
-        width={1558}
-        height={946}
-        unoptimized
-      />
+      {/*<Image*/}
+      {/*  className="absolute left-1/2 top-0 max-w-none -translate-y-1/4 translate-x-[-30%]"*/}
+      {/*  src={backgroundImage}*/}
+      {/*  alt=""*/}
+      {/*  width={1558}*/}
+      {/*  height={946}*/}
+      {/*  unoptimized*/}
+      {/*/>*/}
       <Container className="relative">
         <div className="mx-auto max-w-2xl lg:mx-0">
           <h2
             id="faq-title"
-            className="font-display text-3xl tracking-tight text-slate-900 sm:text-4xl"
+            className="font-display text-3xl tracking-tight text-slate-50 sm:text-4xl"
           >
             FAQ
           </h2>
-          <p className="mt-4 text-lg tracking-tight text-slate-700">
+          <p className="mt-4 text-lg tracking-tight text-slate-100">
             If you have additional questions, don&apos;t hesitate to contact us{" "}
             <a href="mailto:info@billsofknowledge.com" className="underline">
               via email
@@ -158,10 +155,10 @@ export function Faqs() {
               <ul role="list" className="flex flex-col gap-y-8">
                 {column.map((faq, faqIndex) => (
                   <li key={faqIndex}>
-                    <h3 className="font-display text-lg leading-7 text-slate-900">
+                    <h3 className="font-display text-lg leading-7 text-slate-50">
                       {faq.question}
                     </h3>
-                    <p className="mt-4 text-sm text-slate-700">{faq.answer}</p>
+                    <p className="mt-4 text-sm text-slate-300">{faq.answer}</p>
                   </li>
                 ))}
               </ul>
