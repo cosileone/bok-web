b81751bf43f6cbeb551b47299f6ddbc9d4362487
Cosi Leone <cosi.leone@icloud.com>
2024-08-27 15:32:39 +0100

    remove watch video from hero section

--- src/components/salient/Hero.tsx
+++ src/components/salient/Hero.tsx
@@ -49,15 +49,15 @@ export function Hero() {
         <Button href="/register" color="green">
           Try it now
         </Button>
-        <Button href="https://vimeo.com/946554155" color="white">
-          <svg
-            aria-hidden="true"
-            className="h-3 w-3 flex-none fill-lime-600 group-active:fill-current"
-          >
-            <path d="m9.997 6.91-7.583 3.447A1 1 0 0 1 1 9.447V2.553a1 1 0 0 1 1.414-.91L9.997 5.09c.782.355.782 1.465 0 1.82Z" />
-          </svg>
-          <span className="ml-3">Watch Video</span>
-        </Button>
+        {/*<Button href="https://vimeo.com/946554155" color="white">*/}
+        {/*  <svg*/}
+        {/*    aria-hidden="true"*/}
+        {/*    className="h-3 w-3 flex-none fill-lime-600 group-active:fill-current"*/}
+        {/*  >*/}
+        {/*    <path d="m9.997 6.91-7.583 3.447A1 1 0 0 1 1 9.447V2.553a1 1 0 0 1 1.414-.91L9.997 5.09c.782.355.782 1.465 0 1.82Z" />*/}
+        {/*  </svg>*/}
+        {/*  <span className="ml-3">Watch Video</span>*/}
+        {/*</Button>*/}
       </div>
       <div className="mt-36 lg:mt-44">
         <p className="font-display text-base text-slate-100">As seen on</p>
