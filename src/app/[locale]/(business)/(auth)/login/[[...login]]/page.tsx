6c9330486c0e5eafdc257bcfb100457306c17c07
Cosi Leone <cosi.leone@icloud.com>
2024-08-30 10:18:15 +0100

    fix login page to use clerk

--- src/app/[locale]/(business)/(auth)/login/[[...login]]/page.tsx
+++ src/app/[locale]/(business)/(auth)/login/[[...login]]/page.tsx
@@ -1,10 +1,9 @@
 import Link from "next/link";
-
-import { Button } from "~/components/salient/Button";
-import { TextField } from "~/components/salient/Fields";
 import { Logo } from "~/components/salient/Logo";
 import { SlimLayout } from "~/components/salient/SlimLayout";
 import { type Metadata } from "next";
+import { ClerkLoaded, ClerkLoading, SignIn } from "@clerk/nextjs";
+import CircleSpinner from "~/components/CircleSpinner";
 
 export const metadata: Metadata = {
   title: "Sign In",
@@ -13,47 +12,20 @@ export const metadata: Metadata = {
 export default function Login() {
   return (
     <SlimLayout>
-      <div className="flex">
-        <Link href="/public" aria-label="Home">
-          <Logo className="lg:ml-20" />
-        </Link>
-      </div>
-      <h2 className="mt-20 text-lg font-semibold text-gray-900">
-        Sign in to your account
-      </h2>
-      <p className="mt-2 text-sm text-gray-700">
-        Don’t have an account?{" "}
-        <Link
-          href="/register"
-          className="font-medium text-blue-600 hover:underline"
-        >
-          Sign up
-        </Link>{" "}
-        for a free trial.
-      </p>
-      <form action="#" className="mt-10 grid grid-cols-1 gap-y-8">
-        <TextField
-          label="Email address"
-          name="email"
-          type="email"
-          autoComplete="email"
-          required
-        />
-        <TextField
-          label="Password"
-          name="password"
-          type="password"
-          autoComplete="current-password"
-          required
-        />
-        <div>
-          <Button type="submit" variant="solid" color="blue" className="w-full">
-            <span>
-              Sign in <span aria-hidden="true">&rarr;</span>
-            </span>
-          </Button>
+      <Link href="/" aria-label="Home">
+        <Logo className="lg:ml-20" />
+      </Link>
+      <ClerkLoading>
+        <div className={"h-[550px] text-center"}>
+          <CircleSpinner className={"mr-2"} />
+          <span className="text-sm font-medium leading-6 text-gray-900">
+            Contacting Authentication Partner
+          </span>
         </div>
-      </form>
+      </ClerkLoading>
+      <ClerkLoaded>
+        <SignIn forceRedirectUrl={"/dashboard"} />
+      </ClerkLoaded>
     </SlimLayout>
   );
 }
