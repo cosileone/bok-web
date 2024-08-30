2331ba27435eb0caa126c3acc714baca045c64fe
Cosi Leone <cosi.leone@icloud.com>
2024-08-30 12:09:52 +0100

    tie risk assessment form values to graph

--- src/components/InvestmentRadarChart.tsx
+++ src/components/InvestmentRadarChart.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import {
   PolarAngleAxis,
   PolarGrid,
@@ -6,17 +6,65 @@ import {
   RadarChart,
   ResponsiveContainer,
 } from "recharts";
+import { useLocalStorage } from "@uidotdev/usehooks";
 
 // Sample data
-const data = [
-  { subject: "Risk Tolerance", current: 95, fullMark: 100 },
+const sampleData = [
+  { subject: "Risk Tolerance", current: 90, fullMark: 100 },
   { subject: "Age", current: 22, fullMark: 100 },
   { subject: "Saving Rate", current: 50, fullMark: 100 },
   { subject: "Debt Ratio", current: 50, fullMark: 100 },
   { subject: "BOK Score", current: 85, fullMark: 100 },
 ];
 
+type InvestmentRadarChartProps = {
+  riskTolerance: number;
+};
+
 const InvestmentRadarChart: React.FC = () => {
+  const [riskAssessmentAnswers] = useLocalStorage<{
+    riskTolerance: number;
+    debtSituation: string;
+    age: string;
+  }>("bokRiskAssessmentAnswers", {
+    riskTolerance: 5,
+    debtSituation: "",
+    age: "",
+  });
+
+  const [data, setData] = useState(sampleData);
+
+  useEffect(() => {
+    setData((prevData) =>
+      prevData.map((entry) => {
+        switch (entry.subject) {
+          case "Risk Tolerance":
+            return {
+              ...entry,
+              current: riskAssessmentAnswers.riskTolerance * 10,
+            };
+          case "Age":
+            return { ...entry, current: Number(riskAssessmentAnswers.age) };
+          case "Debt Ratio":
+            const debtRatioMap = {
+              "debt-free": 0,
+              "some-debt": 30,
+              "heavy-debt": 100,
+            };
+            return {
+              ...entry,
+              current:
+                debtRatioMap[
+                  riskAssessmentAnswers.debtSituation as keyof typeof debtRatioMap
+                ],
+            };
+          default:
+            return entry;
+        }
+      }),
+    );
+  }, []);
+
   return (
     <ResponsiveContainer width="100%" height={300}>
       <RadarChart cx="50%" cy="50%" outerRadius="63%" data={data}>
